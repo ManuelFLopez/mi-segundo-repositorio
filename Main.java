203fb475f7d25b6e41acb520795f1cf59836dcf4
ManuelFLopez <lopez.inigo.manuel.francisco@iescamas.es>
2026-09-22 10:54:23 +0200

    Modificade desde VS Code

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
    public static void main(String[] args) {
       System.out.println("Hello, World!");
 	System.out.println("Hola, Mundo!");
+   System.out.println("Saludos desde VS Code!");
    }
 }
